8f1b1e9fdfbf9117f6b1fec5419c80a77c4edade
Darkcoffe1859 <youremail@example.com>
2023-08-25 19:43:22 +0100

    puppet

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+package { 'Flask':
+  ensure => '2.1.0',
+  provider => 'pip',
+}
+
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin'],
+  onlyif  => 'pgrep -f killmenow',
+}
+
